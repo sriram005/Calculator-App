c0e82836ab5f17de359b01aeb56250c49bc36bc2
Sriramakrishnan S <sriramakrishnan005@gmail.com>
2023-08-11 22:18:54 +0530

    Calculator App Completed

---
+++ app/src/main/java/com/mofosoft/calculator/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.mofosoft.calculator.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFF27252C)
+val PurpleGrey80 = Color(0xFF27252C)
+val Pink80 = Color(0xFF27252C)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val MediumGray = Color(0xFF2E2E2E)
+val Orange = Color(0xFFFF9800)
